9d590949c1899fd496891676f22fbcfbf260cfe7
ibrohim008 <sunnat5809@gmail.com>
2025-12-06 23:59:25 +0500

    .

--- js/section/categoreSlider.js
+++ js/section/categoreSlider.js
@@ -32,10 +32,7 @@ $(function () {
     responsive: [
       {
         breakpoint: 768,
-        settings: {
-          arrows: false,
-          dots: true
-        }
+        settings: {}
       }
     ]
   });
